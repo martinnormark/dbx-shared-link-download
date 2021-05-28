37bc8512468c172ca4357bdd12c062c4f040949b
Martin H. Normark <m@martinnormark.com>
2021-05-28 20:01:37 +0000

    Call dropbox API to get shared link files

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,30 @@
-import { Dropbox } from 'dropbox';
+import { Dropbox, sharing } from 'dropbox';
 
-const sharedLink = '';
+const sharedLink = '<shared_link_uri>';
 
 console.log('Will check shared link');
 
-const dbx = new Dropbox({ accessToken: '' });
+const dbx = new Dropbox({ accessToken: process.env.DBX_ACCESS_TOKEN });
 
-const sharedMetadata = await dbx.sharingGetSharedLinkMetadata()
+console.log('Auth done');
+
+const metadataCall = dbx.sharingGetSharedLinkMetadata({ url: sharedLink })
+    .then((data: any) => {
+        console.log(data);
+
+        const filesRequest = dbx.filesListFolder({ 
+            path: '',
+            recursive: false, // not supported for shared links
+            shared_link: { url: sharedLink }
+        })
+        .then((files: any) => {
+            console.log('shared files', files.entries)
+        })
+        .catch((err: any) => console.error(err));
+    })
+    .catch((err: any) => {
+        console.error(err);
+    });
+
+Promise.all([metadataCall])
+    .then(() => console.log('Done that'));
